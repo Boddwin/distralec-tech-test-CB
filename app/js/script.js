8bf1ab9dd900057f69cc80e8a1b69d3ca2a2d136
Chris Boddy <chris.boddy@perfecthome.co.uk>
2021-07-04 14:57:03 +0100

    WIP editing js to work with json data

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,26 @@ console.log("Load script.js");
 
 // Instantiating the global app object
 var app = {};
+
+
+$(document).ready(function () {
+    var itemClass;
+    $.getJSON('./src/data.json', {get_param: 'value'}, function (getArray) {
+        $.each(getArray.data, function (index, element) {
+            var totalItems = $(".glide__slide").length;
+            if(totalItems === 0) {
+                itemClass = "glide__slide active";
+            }
+            else {
+                itemClass = "glide__slide";
+            }
+            console.log(totalItems);
+            var thisImage = element.productImageUrl;
+            $('.glide__slides').append('<li class="'+ itemClass +'">' +
+                '<img class="d-block w-100" src="' + thisImage + '" alt="'+ element.productImageAltText +'">' +
+                '<div class="carousel-caption">' +
+                '<h3>' + element.name + '</h3>' +
+                '</div>' + '</li>');
+        });
+    });
+})
